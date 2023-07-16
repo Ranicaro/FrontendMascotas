a931f70d893b87ca6188985c554665990ff0a1a5
JuanMaldonado95 <juan.maldonado-r@hotmail.com>
2023-07-16 14:02:15 -0500

    Actualiza datos en el Mascota

--- src/app/Services/usuarios.service.ts
+++ src/app/Services/usuarios.service.ts
@@ -6,12 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class UsuariosService {
 
-  private apiUrl = 'https://api.example.com/usuarios'; 
+  private apiUrl = 'https://api.example.com/';
 
   constructor(private http: HttpClient) { }
 
   getAllUsuarios() {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>("https://localhost:7187/GetListarUsuarios");
   }
 
   getUsuarioById(id: number) {
@@ -19,8 +19,8 @@ export class UsuariosService {
     return this.http.get<any>(url);
   }
 
-  createUsuario(crearUsuario:Product) {
-    return this.http.post<any>("localhost:44334/PostCrearUsuarios", crearUsuario);
+  createUsuario(crearUsuario: Duennos) {
+    return this.http.post<any>("https://localhost:7187/PostCrearUsuarios", crearUsuario);
   }
 
   updateUsuario(id: number, usuario: any) {
@@ -35,7 +35,8 @@ export class UsuariosService {
 
 }
 
-export interface Product {
+export interface Duennos {
+  iIDUsuario: number
   iIDTipoIdentificacion: number | null;
   tNumeroIdentificacion: string;
   tPrimerNombre: string;
@@ -48,65 +49,70 @@ export interface Product {
   tContrasenna: string;
 }
 
-export const products: Product[] = [
+export const duennos: Duennos[] = [
   {
-      iIDTipoIdentificacion: 1,
-      tNumeroIdentificacion: '1234567890',
-      tPrimerNombre: 'John',
-      tSegundoNombre: 'Doe',
-      tPrimerApellido: 'Smith',
-      tSegundoApellido: 'Johnson',
-      tNumeroTelefono: '1234567890',
-      tDireccion: '123 Main St',
-      tCorreos: 'john@example.com',
-      tContrasenna: 'password1'
+    iIDUsuario: 1,
+    iIDTipoIdentificacion: 1,
+    tNumeroIdentificacion: '1234567890',
+    tPrimerNombre: 'John',
+    tSegundoNombre: 'Doe',
+    tPrimerApellido: 'Smith',
+    tSegundoApellido: 'Johnson',
+    tNumeroTelefono: '1234567890',
+    tDireccion: '123 Main St',
+    tCorreos: 'john@example.com',
+    tContrasenna: 'password1'
   },
   {
-      iIDTipoIdentificacion: 2,
-      tNumeroIdentificacion: '0987654321',
-      tPrimerNombre: 'Jane',
-      tSegundoNombre: 'Doe',
-      tPrimerApellido: 'Johnson',
-      tSegundoApellido: 'Smith',
-      tNumeroTelefono: '9876543210',
-      tDireccion: '456 Elm St',
-      tCorreos: 'jane@example.com',
-      tContrasenna: 'password2'
+    iIDUsuario: 1,
+    iIDTipoIdentificacion: 2,
+    tNumeroIdentificacion: '0987654321',
+    tPrimerNombre: 'Jane',
+    tSegundoNombre: 'Doe',
+    tPrimerApellido: 'Johnson',
+    tSegundoApellido: 'Smith',
+    tNumeroTelefono: '9876543210',
+    tDireccion: '456 Elm St',
+    tCorreos: 'jane@example.com',
+    tContrasenna: 'password2'
   },
   {
-      iIDTipoIdentificacion: 3,
-      tNumeroIdentificacion: '9876543210',
-      tPrimerNombre: 'Mike',
-      tSegundoNombre: 'Doe',
-      tPrimerApellido: 'Brown',
-      tSegundoApellido: 'Miller',
-      tNumeroTelefono: '5678901234',
-      tDireccion: '789 Oak St',
-      tCorreos: 'mike@example.com',
-      tContrasenna: 'password3'
+    iIDUsuario: 1,
+    iIDTipoIdentificacion: 3,
+    tNumeroIdentificacion: '9876543210',
+    tPrimerNombre: 'Mike',
+    tSegundoNombre: 'Doe',
+    tPrimerApellido: 'Brown',
+    tSegundoApellido: 'Miller',
+    tNumeroTelefono: '5678901234',
+    tDireccion: '789 Oak St',
+    tCorreos: 'mike@example.com',
+    tContrasenna: 'password3'
   },
   {
-      iIDTipoIdentificacion: 4,
-      tNumeroIdentificacion: '4567890123',
-      tPrimerNombre: 'Sarah',
-      tSegundoNombre: 'Doe',
-      tPrimerApellido: 'Wilson',
-      tSegundoApellido: 'Davis',
-      tNumeroTelefono: '8901234567',
-      tDireccion: '321 Pine St',
-      tCorreos: 'sarah@example.com',
-      tContrasenna: 'password4'
+    iIDUsuario: 1,
+    iIDTipoIdentificacion: 4,
+    tNumeroIdentificacion: '4567890123',
+    tPrimerNombre: 'Sarah',
+    tSegundoNombre: 'Doe',
+    tPrimerApellido: 'Wilson',
+    tSegundoApellido: 'Davis',
+    tNumeroTelefono: '8901234567',
+    tDireccion: '321 Pine St',
+    tCorreos: 'sarah@example.com',
+    tContrasenna: 'password4'
   },
   {
-      iIDTipoIdentificacion: 5,
-      tNumeroIdentificacion: '2345678901',
-      tPrimerNombre: 'Chris',
-      tSegundoNombre: 'Doe',
-      tPrimerApellido: 'Taylor',
-      tSegundoApellido: 'Anderson',
-      tNumeroTelefono: '4567890123',
-      tDireccion: '567 Cedar St',
-      tCorreos: 'chris@example.com',
-      tContrasenna: 'password5'
+    iIDUsuario: 1,
+    iIDTipoIdentificacion: 5,
+    tNumeroIdentificacion: '2345678901',
+    tPrimerNombre: 'Chris',
+    tSegundoNombre: 'Doe',
+    tPrimerApellido: 'Taylor',
+    tSegundoApellido: 'Anderson',
+    tNumeroTelefono: '4567890123',
+    tDireccion: '567 Cedar St',
+    tCorreos: 'chris@example.com',
+    tContrasenna: 'password5'
   },
 ];
